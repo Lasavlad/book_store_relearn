0eb4b9f4b9339427499adfda2e2913decc0b97c1
Lasavlad <98495511+Lasavlad@users.noreply.github.com>
2023-02-24 13:02:49 +0100

    ch12

--- books/tests.py
+++ books/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.urls import reverse
 
 from .models import Book, Review
@@ -24,6 +25,8 @@ class BookTests(TestCase):
             password = 'testpassword'
         )
 
+        self.special_permission = Permission.objects.get(codename='special_status')
+
         self.review = Review.objects.create(
             book = self.book,
             author = self.user,
@@ -38,12 +41,26 @@ class BookTests(TestCase):
         self.assertEqual(f'{self.book.author}', 'Jk Rowling')
         self.assertEqual(f'{self.book.price}', '25.00')
 
-    def test_book_list_view(self):
-        self.assertEqual(self.response.status_code, 200)
-        self.assertContains(self.response, 'Harry Potter')
-        self.assertTemplateUsed(self.response, 'books/book_list.html')
+    def test_book_list_view_for_logged_in_user(self):
+        self.client.login(email="testemail@email.com", password='testpassword')
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Harry Potter')
+        self.assertTemplateUsed(response, 'books/book_list.html')
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_logged_out_user(self):
+        self.client.logout()
+        self.assertEqual(self.response.status_code, 302)
+        self.assertRedirects(self.response, '%s?next=/books/' % (reverse('account_login')))
+        response = self.client.get(
+            '%s?next=/books/' % (reverse('account_login'))
+        )
+        self.assertContains(response, 'Login')
+
+
+    def test_book_detail_view_with_permission(self):
+        self.client.login(email="testemail@email.com", password="testpassword")
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get('/book/12345/')
         self.assertEqual(response.status_code, 200)
